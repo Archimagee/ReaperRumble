fed558cbdbef0e5c8e27d96ebffd7b690836dd94
Archimagee (Laptop) <jr2g22@soton.ac.uk>
2025-02-24 14:12:37 +0000

    Fix orphaned group position

--- Assets/Scripts/ECS/Systems/OrphanSoulsClientSystem.cs
+++ Assets/Scripts/ECS/Systems/OrphanSoulsClientSystem.cs
@@ -6,6 +6,7 @@ using Unity.Physics.Systems;
 using Unity.NetCode;
 using Unity.Physics;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 
 
@@ -70,6 +71,7 @@ public partial struct OrphanSoulsClientSystem : ISystem
 
 
             if (!SystemAPI.HasBuffer<SoulBufferElement>(_newGroup)) ecb.AddBuffer<SoulBufferElement>(_newGroup);
+            SystemAPI.SetComponent(_newGroup, new LocalTransform() { Position = SystemAPI.GetComponent<LocalTransform>(_groupToOrphanFrom).Position })
             foreach (Entity soulEntity in soulsToMove)
             {
                 ecb.SetComponent(soulEntity, new SoulGroupMember() { MyGroup = _newGroup });
